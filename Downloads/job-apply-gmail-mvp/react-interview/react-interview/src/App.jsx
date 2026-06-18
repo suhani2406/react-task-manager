9dbec935a14e591feeb514125966c3c03213450f
suhani2406 <suhanisuniltiwari@gmail.com>
2026-06-18 17:05:17 +0530

    Completed React Task Manager

---
+++ Downloads/job-apply-gmail-mvp/react-interview/react-interview/src/App.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const savedTasks =
+      JSON.parse(localStorage.getItem("tasks")) || [];
+    setTasks(savedTasks);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = () => {
+    if (!task.trim()) return;
+
+    const newTask = {
+      id: Date.now(),
+      title: task,
+    };
+
+    setTasks([...tasks, newTask]);
+    setTask("");
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const filteredTasks = tasks.filter((task) =>
+    task.title.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Task Manager</h1>
+
+        <div className="add-task">
+          <input
+            type="text"
+            placeholder="Enter Task"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+
+          <button onClick={addTask}>Add Task</button>
+        </div>
+
+        <input
+          className="search"
+          type="text"
+          placeholder="Search Task"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <h3>Total Tasks: {tasks.length}</h3>
+
+        <div className="task-list">
+          {filteredTasks.length > 0 ? (
+            filteredTasks.map((task) => (
+              <div className="task-item" key={task.id}>
+                <span>{task.title}</span>
+
+                <button
+                  className="delete-btn"
+                  onClick={() => deleteTask(task.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            ))
+          ) : (
+            <p>No Tasks Found</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
